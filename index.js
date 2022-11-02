800c7845b4edf6d2bce74d217f4c95fa330dbb3f
Kassandra <K1995Moreno@gmail.com>
2022-11-02 18:43:39 -0400

    commit tree 17a40b06d55cc5f9c2aa892457ee4bdb1e9f7461

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your code here
+
+
+//returns what key is pressed
+/*document.addEventListener("keydown", function (event) {
+    console.log(event);
+});*/
+
+const dodger= document.getElementById('dodger')
+dodger.style.backgroundColor = 'rgba(6, 236, 156)'
+dodger.style.bottom = "0px"
+dodger.style.left = "0px"
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
